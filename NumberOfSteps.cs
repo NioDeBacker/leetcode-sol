4097a52cedc72134c6e1b1936847e8cbaa8369d3
Nio De Backer <79175757+NioDeBacker@users.noreply.github.com>
2023-05-25 22:31:11 +0200

    Create NumberOfSteps.cs

---
+++ NumberOfSteps.cs
@@ -0,0 +1,14 @@
+public class Solution {
+    public int NumberOfSteps(int num) {
+        int step = 0;
+        while (num > 0) {
+            if (num % 2 == 0) {
+                num = num / 2;
+            } else {
+                num = num - 1;
+            }
+            step += 1;
+        }
+        return step;
+    }
+}
